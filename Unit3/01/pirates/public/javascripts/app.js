284cfea7943852e2c02f34ef2b629b63294c54ed
Alan Taylor <alancarltaylor@gmail.com>
2016-05-09 11:57:09 -0600

    hello world displays

---
+++ Unit3/01/pirates/public/javascripts/app.js
@@ -0,0 +1,7 @@
+angular.module('pirates', [])
+.controller("ctrl", ["$scope", function($scope){
+
+  $scope.vm = {};
+  $scope.vm.message = "Hello World"
+
+}])
---
+++ Unit3/01/pirates/public/javascripts/controllers/main_ctrl.js
@@ -0,0 +1,6 @@
+// angular.module("pirates").controller("ctrl", ["$scope", function($scope){
+//
+//   $scope.vm = {};
+//   $scope.vm.message = "Hello World"
+//
+// }])
